805fd2d3381bb44a6467b0eb603bf849b2cf8ff3
scarviz <scarviz@gmail.com>
2017-11-04 16:41:36 +0900

    戦艦シミュレータプロジェクト新規追加

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public Transform m_cannonRot;
+	public Transform m_muzzle;
+	public GameObject m_shotPrefab;
+	public float coefficient = 1f;
+
+	private const float INTERVAL = 3f;
+	private float elapsed = 0f;
+	private float randomVal = 0f;
+
+	// Use this for initialization
+	void Start () {
+		if(coefficient == 0)
+		{
+			coefficient = 1f;
+		}
+
+		randomVal = RandomVal();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		elapsed += Time.deltaTime * coefficient * randomVal;
+		if (INTERVAL < elapsed)
+		{
+			GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
+			GameObject.Destroy(go, 3f);
+			elapsed = 0;
+			randomVal = RandomVal();
+		}
+	}
+
+	private float RandomVal()
+	{
+		return Random.Range(0.5f, 1.5f);
+	}
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Shot : MonoBehaviour {
+	private const string REQUEST_URL = "https://blooming-brushlands-57006.herokuapp.com/pulldata";
+
+	public Transform m_cannonRot;
+	public Transform m_muzzle;
+	public GameObject m_shotPrefab;
+
+	private class Message
+	{
+		public string intent;
+	}
+
+	private class MessType
+	{
+		public const string SHOT = "shot";
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		StartCoroutine(GetText());
+	}
+
+	IEnumerator GetText()
+	{
+		var request = UnityWebRequest.Get(REQUEST_URL);
+
+		// リクエスト送信
+		yield return request.Send();
+
+		// 通信エラーチェック
+		if (request.isError)
+		{
+			Debug.Log(request.error);
+		}
+		else if (request.responseCode == 200)
+		{
+			// UTF8文字列として取得する
+			var json = request.downloadHandler.text;
+			Debug.Log(json);
+
+			if (string.IsNullOrEmpty(json) || json == "null") yield return null;
+
+			var mes = LitJson.JsonMapper.ToObject<Message>(json);
+			if (mes == null || string.IsNullOrEmpty(mes.intent)) yield return null;
+
+			switch (mes.intent)
+			{
+				case MessType.SHOT:
+					shot();
+					break;
+				default:
+					break;
+			}
+
+		}
+	}
+
+	private void shot()
+	{
+		GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
+		GameObject.Destroy(go, 3f);
+	}
+}
